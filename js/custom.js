cb426dc8535011f554d8a5d27fa370d0fed66962
tobywynne-mellor <t.wynne-mellor@se17.qmul.ac.uk>
2018-07-25 03:18:01 +0100

    Portfolio 3.0

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+
+    var feed = new Instafeed({
+        get: 'user',
+        accessToken: '195697929.1677ed0.d876b5ec959648de8137b14601591839',
+        userId: '195697929',
+        limit: 2,
+        resolution: "low_resolution",
+        template: '<img class="instaPic" src="{{image}}" alt="instafeed_img"/>',
+        after: function () {
+            $( "img:eq(1)" ).addClass('instapicone');
+            $( "img:eq(2)" ).addClass('instapictwo');
+            console.log("done");
+        
+        }
+        // template: '<div class="card"><img class="card-img instafeed_img" src="{{image}}" alt="Card image"></div>',
+        // after: function () {
+        //     $('.card').each(function(i){
+        //         setTimeout(function(){
+        //             $('.card').eq(i).addClass('is-showing2');
+        //         },250*(i + 1));
+        //     });
+        // }
+    });
+    feed.run();
+
+});
+
+//https://codeofaninja.com/tools/find-instagram-user-id
+//http://instagram.pixelunion.net/
+
+//access token 195697929.1677ed0.d876b5ec959648de8137b14601591839
+//user id 5787145626
--- js/index.js
+++ js/index.js
@@ -1,26 +1,7 @@
 $(document).ready(function(){
-
-
-  //typewriter
-
-  var str = "<h1>TOBY WYNNE-MELLOR</h1>",
-      i = 0,
-      isTag,
-      text;
-
-  (function type() {
-    text = str.slice(0, ++i);
-    if (text === str) return;
-
-    document.getElementById('title').innerHTML = text;
-
-    var char = text.slice(-1);
-    if( char === '<' ) isTag = true;
-    if( char === '>' ) isTag = false;
-
-    if (isTag) return type();
-    setTimeout(type, 80);
-  }());
-
-
+  $('.item').each(function(i){
+    setTimeout(function(){
+      $('.item').eq(i).addClass('is-showing');
+    },150*(i+1));
+  });
 });
