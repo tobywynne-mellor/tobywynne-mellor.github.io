307d2bc27b2eccd7276bcb14ef0c1218bcadeed8
Toby <tobytogs@hotmail.com>
2018-07-30 17:27:07 +0100

    added about page

--- index.php
+++ index.php
@@ -16,6 +16,10 @@ switch ($request_uri[0]) {
     case '/projects':
         header('Location: https://tobywynne-mellor.github.io/projects.html');
         break;
+    //about page
+    case '/about':
+        header('Location: http://tobywynnemellor.com/about.html');
+        break;
     // Everything else
     default:
         header('HTTP/1.0 404 Not Found');
