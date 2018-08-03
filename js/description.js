a01ca79dbd2d4f13d75216bcb5b4b9c07b33a551
Toby <tobytogs@hotmail.com>
2018-08-03 01:35:44 +0100

    description page fixes

--- js/description.js
+++ js/description.js
@@ -1,10 +1,10 @@
 var param = window.location.search;
-param.split('').splice(0, 1); // removes ?
 
 var heading = document.getElementById('heading');
 var img = document.getElementById('img');
 var description = document.getElementById('description');
 var details = document.getElementById('details');
+var link = document.getElementById('link');
 
 var projects = [
     {
@@ -12,14 +12,16 @@ var projects = [
         "heading": "Stock Trading Simulator",
         "img": "images/trading.png",
         "description": "An object-orientated desktop stock trading simulator in Java + JavaFX. I used JavaFX for the UI.",
-        "details": '<span class="tag">Java <i class="fab fa-java"></i></span>&nbsp;&nbsp;<span class="tag">Desktop <i class="fas fa-desktop"></i></span>'
+        "details": '<span class="tag">Java <i class="fab fa-java"></i></span>&nbsp;&nbsp;<span class="tag">Desktop <i class="fas fa-desktop"></i></span>',
+        "link": "https://github.com/tobywynne-mellor/trading-simulator"
     },
     {
         "id": "?kanye-sentiment-analysis",
-        "heading": "Stock Trading Simulator",
+        "heading": "Kanye Sentiment Analysis",
         "img": "images/kanye.png",
         "description": "I used Node, the Twitter API and IBM Wason Tone Analyzer API to create a bot which analyses Kanye's tweets. I then hosted the bot on Heroku and waited to Kanye to tweet!",
-        "details": "<li>Node</li><li>Java</li>"
+        "details": '<span class="tag">Node <i class="fab fa-node"></i></span>&nbsp;&nbsp;<span class="tag">Cloud <i class="fas fa-cloud"></i></span>',
+        "link": "https://github.com/tobywynne-mellor/kanye-sentiment-analysis"
     }
 ];
 console.log("project: " + param);
@@ -30,6 +32,7 @@ for (var i = 0; i < projects.length; i++) {
         img.src = projects[i].img;
         description.innerHTML = projects[i].description;
         details.innerHTML = projects[i].details;
+        link.href = projects[i].link;
     }
 }
 
